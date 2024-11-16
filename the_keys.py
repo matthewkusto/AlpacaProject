ee8c6728b55702d0b9acfd88d1b4b4e6dd0566c4
matthewkusto <mkusto.2001@gmail.com>
2024-11-15 18:01:08 -0700

    keys

--- the_keys.py
+++ the_keys.py
@@ -1,2 +1,2 @@
-api_key = "PKJID1VGYJ85RK6DWP80"
-secret_api_key = "VWkDI3q7FRKLW3EcyjAitZ5tIBoTywnL5RQtUMh6"
+api_key = "PKL3OMNYYX2DUQDTENLJ"
+secret_api_key = "6EBx9D5xgP3iawrdMNP8YM9LCyuSAXSJo5XgoypZ"
